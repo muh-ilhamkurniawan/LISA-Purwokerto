d57f4e6a9de8d4687b5cf3ae9ad8d2db16e75ed9
muh-ilhamkurniawan <93339557+muh-ilhamkurniawan@users.noreply.github.com>
2024-06-21 09:08:13 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,101 @@
+import streamlit as st
+from streamlit_chat import message as st_message
+
+# Definisikan pasangan pertanyaan dan jawaban
+qa_pairs = {
+    "halo": "Halo selamat datang di KPw Bank Indonesia Purwokerto, silahkan pilih informasi yang diinginkan:\n1. Tentang Bank Indonesia\n2. Informasi Kantor Perwakilan Bank Indonesia Purwokerto\n3. Layanan yang tersedia di Bank Indonesia Purwokerto\n4. Magang dan PKL\n5. Selesai",
+
+    "1": "Bank Indonesia (BI) adalah bank sentral Republik Indonesia yang bertanggung jawab dalam mengatur dan menjaga kestabilan nilai rupiah. BI juga memiliki peran penting dalam pengaturan kebijakan moneter untuk mencapai tujuan ekonomi nasional.\nSilakan pilih informasi lebih lanjut:\n1. Tugas dan Fungsi\n2. Sejarah BI\n3. Visi dan Misi\n4. Tujuan dan Tugas\n5. Kontak\n6. Batal",
+        "1.1": "Tugas dan Fungsi Bank Indonesia adalah menetapkan dan melaksanakan kebijakan moneter, mengatur dan menjaga kelancaran sistem pembayaran, serta mengatur dan mengawasi perbankan di Indonesia.",
+        "1.2": "Sejarah Bank Indonesia dimulai pada tahun 1953, saat bank ini pertama kali didirikan sebagai bank sentral Republik Indonesia.",
+        "1.3": "Visi dan Misi Bank Indonesia.",
+        "1.4": "Tujuan dan Tugas Bank Indonesia.",
+        "1.5": "Bank Indonesia\nJalan M.H. Thamrin No. 2, Jakarta 10350\nContact Center Bank Indonesia Bicara\nTelp. : 131 dan 1500131 (dari luar negeri)\nE-mail : bicara@bi.go.id\nChatbot LISA : 081 131 131 131.",
+
+    "2": "Silakan pilih informasi lebih lanjut:\n1. Lokasi\n2. Jam Pelayanan\n3. Kontak\n4. Batal",
+        "2.1": "Berikut adalah alamat KPwBI Purwokerto :\nJl. Jend. Gatot Subroto No.98, Brubahan, Purwanegara, Kec. Purwokerto Tim., Kabupaten Banyumas, Jawa Tengah 53116\nGoogle Maps: https://g.co/kgs/qe14jYA",
+        "2.2": "Jam Pelayanan KPwBI Purwokerto: Senin - Jumat, 08:00 - 16:00 WIB",
+        "2.3": "Anda bisa menghubungi KPwBI Purwokerto di kontak berikut: \n- Instagram : https://www.instagram.com/bank_indonesia_purwokerto/\n- TikTok : https://www.tiktok.com/@bi.purwokerto\n- Twitter : https://twitter.com/BI_Purwokerto\n- Youtube : https://www.youtube.com/@bankindonesiapurwokerto702",
+
+    "3": "Silakan pilih informasi lebih lanjut:\n1. Lokasi\n2. Jam Pelayanan\n3. Kontak\n4. Batal",
+    
+    "4": "PKL adalah kegiatan praktik kerja yang diberikan kepada mahasiswa/siswa yang difasilitasi oleh Bank Indonesia. Memberikan kesempatan bagi mahasiswa/sis​wa untuk belajar dan mengembangkan diri melalui keterlibatan langsung dalam pelaksanaan tugas di Bank Indonesia. Silahkan pilih informasi yang diinginkan:\n1. Persyaratan Umum Akademik\n2. Alur Pendaftaran\n3. Persyaratan Administrasi & Permohonan\n4. Batal",
+        "4.1": "Persyaratan Umum Akademik :\na. Jenjang pendidikan:\n- Peserta PKL: D3/D4/S1/S2\n- Peserta PKL: Sekolah Menengah Kejuruan (SMK)\n\nb. Tingkat pendidikan:\n- Peserta PKL, minimal semester 6\n- Peserta PKL, minimal kelas XI\n\nc. Bidang Studi:\n- Peserta PKL: Ekonomi (Manajemen, Akuntansi, Ilmu Ekonomi, Keuangan), Matematika, Statistika, Teknik Industri, Teknik Informatika, Ilmu Komputer, Sistem Informasi, Hukum, Administrasi Bisnis/Niaga, Psikologi.\n- Peserta PKL: Semua jurusan yang tersedia di SMK.\n\nd. Keahlian khusus antara lain:\n- Peserta PKL: Menguasai Microsoft Office (Word, Excel, PowerPoint); desain grafis; programmer;\n- Peserta PKL: komputer jaringan, multimedia; administrasi arsip; menguasai Microsoft Office (Word, Excel, PowerPoint).",
+        "4.2": "Alur Pendaftaran :\n- Pengajuan Magang melalui surat pengantar dan proposal yang dikirimkan ke Kantor Perwakilan Bank Indonesia Purwokerto\n- Jangka waktu proses seleksi maksimal 1 bulan\nJika Lolos : Akan dihubungi pihak KPwBI Purwokerto\nJika Tidak Lolos : Informasi melalui telepon (0281) 631631 KPwBI\n\nCatatan :\n1. Jika mahasiswa lolos seleksi akan dihubungi oleh pihak Bank Indonesia Purwokerto\n2. Pengiriman surat pengantar dan proposal paling lambat 3 bulan sebelum periode magang yang dikehendaki\n3. Seluruh dokumen surat pengantar dan proposal adalah dokumen asli, dikirim ke Bank Indonesia Purwokerto (dalam bentuk hardcopy). Tidak melayani email.",
+        "4.3": "Persyaratan Administrasi & Permohonan :\n- Surat Pengantar dari Universitas/Sekolah. Mencakup:\n  - Keterangan data mahasiswa/siswa (Nama, NIM/NIS, Fakultas/Program Studi/Jurusan, Semester/Kelas)\n  - Durasi dan Periode PKL\n- Fotokopi transkrip nilai semester terakhir\n- Proposal Individu. Mencakup:\n  - Data diri lengkap (CV)\n  - Motivation Letter (menjelaskan maksud dan tujuan PKL, harapan atau target yang akan dicapai).\n  - Bidang pekerjaan yang diminati (menceritakan passion atau minat terhadap salah satu bidang pekerjaan: moneter & makroprudensial, sistem pembayaran, pengelolaan uang rupiah, manajemen intern)\n  - Informasi bidang tugas dapat dilihat di tautan berikut ini.\n  - Fotokopi KTP\n  - Fotokopi NPWP\n  - Fotokopi buku rekening tabungan pribadi (khusus untuk peserta PKL).",
+
+    "5": "Silakan pilih layanan lebih lanjut:\n1. Pengaduan\n2. Pengisian Survey Kepuasan\n3. Batal",
+        "5.1": "Silakan isi pengaduan pengguna pada tautan berikut : bit.ly/....",
+        "5.2": "Silakan isi survey kepuasan pengguna pada tautan berikut : bit.ly/....",
+
+    "menu": "Halo selamat datang di KPw Bank Indonesia Purwokerto, silahkan pilih informasi yang diinginkan:\n1. Tentang Bank Indonesia\n2. Informasi Kantor Perwakilan Bank Indonesia Purwokerto\n3. Layanan yang tersedia di Bank Indonesia Purwokerto\n4. Magang dan PKL\n5. Selesai",
+
+    "lanjutan": "Adakah hal lain yang bisa Lisa bantu lagi? Silakan ketik 'Menu' untuk kembali ke menu utama atau ketik 'Selesai' untuk mengakhiri percakapan ini yaa 😊",
+
+    "batal": "Adakah hal lain yang bisa Lisa bantu lagi? Silakan ketik 'Menu' untuk kembali ke menu utama atau ketik 'Selesai' untuk mengakhiri percakapan ini yaa 😊",
+
+    "selesai": "Terimakasih sudah menggunakan layanan LISA. Jika ada yang mau ditanyakan silahkan ketik 'Halo'😊"
+}
+
+# Menambahkan kunci-kunci lowercase yang sesuai
+for key in list(qa_pairs.keys()):
+    if key.lower() not in qa_pairs:
+        qa_pairs[key.lower()] = qa_pairs[key]
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+    st.session_state.current_node = "halo"
+    st.session_state.current_sequence = ""
+
+st.title("Hello Chatbot")
+
+# Tampilkan riwayat chatbot di area utama (body) Streamlit
+for i, chat in enumerate(st.session_state.history):
+    st_message(**chat, key=str(i))
+
+# Membuat area kosong untuk input teks
+input_area = st.empty()
+
+def generate_answer():
+    user_message = st.session_state.input_text.lower()  # Konversi ke lowercase
+    current_node = st.session_state.current_node
+    current_sequence = st.session_state.current_sequence
+
+    if user_message == "selesai":
+        next_node = "selesai"  # Mengatur kembali ke awal
+        current_sequence = ""  # Mengosongkan sequence
+    else:
+        # Update node berdasarkan input
+        # if user_message.isdigit() and user_message not in ["halo", "lanjutan", "menu"]:
+        if user_message.isdigit() and user_message not in ["halo", "lanjutan", "menu"]:
+            if current_sequence == "":
+                current_sequence = user_message
+                next_node = user_message
+            else :
+                next_node = f"{current_sequence}.{user_message}"
+        else:
+            next_node = user_message
+
+    # Cari jawaban berdasarkan pertanyaan yang telah didefinisikan
+    message_bot = qa_pairs.get(next_node, "Maaf, saya tidak mengerti pertanyaan Anda.")
+    
+    # Tambahkan pesan pengguna ke riwayat
+    st.session_state.history.append({"message": user_message, "is_user": True})
+    # Tambahkan jawaban chatbot ke riwayat
+    # st.session_state.history.append({"message": (f"current_sequence = {current_sequence}"), "is_user": False})
+    # st.session_state.history.append({"message": (f"next_node = {next_node}"), "is_user": False})
+    st.session_state.history.append({"message": message_bot, "is_user": False})
+    
+    # Tambahkan pesan lanjutan jika diperlukan
+    if next_node in ["1.1", "1.2", "1.3", "1.4", "1.5", "2.1", "2.2", "2.3", "3", "4.1", "4.2", "4.3", "5.1", "5.2"]:
+        st.session_state.history.append({"message": qa_pairs["lanjutan"], "is_user": False})
+    elif next_node == "menu":
+        next_node = "halo"
+    
+    # Update current node dan sequence di state
+    st.session_state.current_node = next_node
+    st.session_state.current_sequence = current_sequence if user_message != "selesai" else ""
+
+# Menempatkan input teks
+input_area.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
+
